cd018d626d27742373972158e15f0c60f0c2425b
SMK-hub <75328575+SMK-hub@users.noreply.github.com>
2024-02-28 09:50:42 +0530

    Update Train1Application.java

--- src/main/java/com/example/Demo/Train1Application.java
+++ src/main/java/com/example/Demo/Train1Application.java
@@ -27,7 +27,7 @@ public class Train1Application {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://65.2.40.156:3000","http://65.2.40.156:3001")
+						.allowedOrigins("http://13.201.51.40:3000","http://13.201.51.40:3001")
 						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.allowedHeaders("*");
 			}
